cefc007bc028d4f85777f588f3f3486e24c658a4
Gabriel Stellini <gabrielstellini@gmail.com>
2018-11-07 12:32:20 +0100

    Add bitbucket deploy test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import React from "react";
 const Header = () => {
     return (
         <div>
+            <h1>If you can see this, deploying through Bitbucket works!</h1>
         </div>
     );
 };
