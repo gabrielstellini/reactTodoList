7b9256e4a7e015289ea0f4678e6593577a56a85b
Gabriel Stellini <gabrielstellini@gmail.com>
2018-11-08 17:31:10 +0100

    Add item CRUD updates

--- src/components/todoList/todoItem/todoItem.js
+++ src/components/todoList/todoItem/todoItem.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import classes from './todoItem.css';
 
-const TodoItem = (props) => {
-    return (
-        <div className={classes.Item}>
-            <h4 className={'text-center'}>{props.text}</h4>
-            <button className={classes.Button} onClick = {(event) => props.deleteItemHandler(event,props.itemId)}>Remove</button>
-        </div>
-    );
-};
+class TodoItem extends PureComponent {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            formValues: {
+                id: props.itemId,
+                value: props.text
+            }
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+                formValues: {
+                    ...this.state.formValues,
+                    value: event.target.value
+                }
+            }
+        );
+
+        console.log(this.state.formValues.id)
+    };
+
+    render() {
+        return (
+            <div className={classes.Item}>
+                <input value={this.state.formValues.value} onChange={this.handleChange} className={classes.Input}/>
+                <button className={classes.Button}
+                        onClick={(event) =>
+                            this.props.updateItemHandler(event, this.props.itemId, this.state.formValues.value)}>Update
+                </button>
+                <button className={classes.Button}
+                        onClick={(event) => this.props.deleteItemHandler(event, this.props.itemId)}>Remove
+                </button>
+
+            </div>
+        );
+    };
+}
 
 export default TodoItem;
